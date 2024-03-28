0801a381e21359b6b6b95f260e128849d12a5e71
Omar E <31523746+omar-estevez@users.noreply.github.com>
2024-03-27 20:09:32 -0500

    fixing bugs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,19 @@
 import './../styles/Nav.scss'
 import imageLogo from '../icons_assets/Logo.svg';
+import { CiMenuKebab } from "react-icons/ci";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 const Nav = () => {
+
+    const [isElementVisible, setIsElementVisible] = useState(false);
+
+    const handleMenuClick = () => {
+        setIsElementVisible(!isElementVisible);
+    };
+
+    const isDesktop = useMediaQuery({ minWidth: 800 });
 
     return (
         <>
@@ -10,26 +21,52 @@ const Nav = () => {
                 <div className='image'>
                     <img src={imageLogo} alt="Little Lemon Logo" />
                 </div>
-                <div className='menu'>
-                    <Link to='/'>
-                        Home
-                    </Link>
-                    <Link>
-                        About
-                    </Link>
-                    <Link>
-                        Menu
-                    </Link>
-                    <Link to="/booking">
-                        Reservations
-                    </Link>
-                    <Link>
-                        Order Online
-                    </Link>
-                    <Link>
-                        Login
-                    </Link>
-                </div>
+                {!isDesktop && ( <CiMenuKebab className='mobile-menu' onClick={handleMenuClick}/>)}
+                {!isDesktop && isElementVisible && ( <hr /> )}
+                {!isDesktop && isElementVisible && (
+                    <div className={`menu ${isElementVisible ? 'visible' : ''}`}>
+                        <Link to='/'>
+                            Home
+                        </Link>
+                        <Link>
+                            About
+                        </Link>
+                        <Link>
+                            Menu
+                        </Link>
+                        <Link to="/booking">
+                            Reservations
+                        </Link>
+                        <Link>
+                            Order Online
+                        </Link>
+                        <Link>
+                            Login
+                        </Link>
+                    </div>
+                )}
+                {isDesktop && (
+                    <div className={`menu ${isElementVisible ? 'visible' : ''}`}>
+                        <Link to='/'>
+                            Home
+                        </Link>
+                        <Link>
+                            About
+                        </Link>
+                        <Link>
+                            Menu
+                        </Link>
+                        <Link to="/booking">
+                            Reservations
+                        </Link>
+                        <Link>
+                            Order Online
+                        </Link>
+                        <Link>
+                            Login
+                        </Link>
+                    </div>
+                )}
             </nav>
         </>
     )
--- src/components/main components/CallToAction.js
+++ src/components/main components/CallToAction.js
@@ -1,7 +1,15 @@
 import '../../styles/CallToAction.scss';
 import imageSection from '../../icons_assets/restauranfood.jpg';
+import { useNavigate } from "react-router-dom";
 
 const CallToAction = () => {
+
+    const navigate = useNavigate();
+
+    const goToReservation = () => {
+        navigate("/booking");
+    }
+
     return (
         <>
             <section className='call-section'>
@@ -9,7 +17,7 @@ const CallToAction = () => {
                     <h1>Little Lemon</h1>
                     <h2>Chicago</h2>
                     <p>We are a family owned Medierranean restaurant, focused on traditional recipes served with a modern twist</p>
-                    <button>Reserve a table</button>
+                    <button onClick={goToReservation}>Reserve a table</button>
                 </div>
                 <div className='image'>
                     <img src={imageSection} alt='table little lemon' />
